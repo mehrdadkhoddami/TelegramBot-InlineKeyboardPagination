5f74739f935c591f39b31465de60a968725dc878
Mehrdad <khoddami.me@gmail.com>
2017-10-17 11:12:48 +0330

    fork from github.com/mehrdadkhoddami/inline-keyboard-pagination to have a package in packagist

---
+++ src/Exceptions/InlineKeyboardPaginationException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MehrdadKhoddami\TelegramBot\InlineKeyboardPagination\Exceptions;
+
+use Exception;
+
+/**
+ * Class InlineKeyboardPaginationException
+ * Based on https://github.com/php-telegram-bot/inline-keyboard-pagination
+ *
+ * @package MehrdadKhoddami\TelegramBot\InlineKeyboardPagination
+ */
+final class InlineKeyboardPaginationException extends Exception
+{
+}
